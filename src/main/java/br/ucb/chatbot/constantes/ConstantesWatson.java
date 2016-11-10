0cc33be90f331090ec763d452ec6882a68ca8cb3
kleitonbatista <kleiton.a.batista@gmail.com>
2016-11-10 14:38:47 -0200

    alterando as credenciais para realizar teste com o bot ucb

--- src/main/java/br/ucb/chatbot/constantes/ConstantesWatson.java
+++ src/main/java/br/ucb/chatbot/constantes/ConstantesWatson.java
@@ -8,8 +8,18 @@ package br.ucb.chatbot.constantes;
  * 
  */
 public class ConstantesWatson {
-	public static final String PASSWORD_CONVERSATION = "23kqErpK4b1b";
-	public static final String USER_NAME_CONVERSATION = "e01c3c9f-47ac-4421-ad27-6a0959f8d080";
+	/**
+	 * Pass Anaclara 23kqErpK4b1b
+	 * user AnaClara e01c3c9f-47ac-4421-ad27-6a0959f8d080
+	 * work AnaClara 600d461a-ac5e-4972-bd18-24bdfb3c07a2
+	 * 
+	 * Pass ucb 814bjrjP0UzA
+	 * user ucb cfc3c51f-0a26-4f2b-99bc-a1cd6450a5c7
+	 * work ucb 263a1f22-4d05-4294-917b-180ee351760f
+	 * 
+	 */
+	public static final String PASSWORD_CONVERSATION = "814bjrjP0UzA";
+	public static final String USER_NAME_CONVERSATION = "cfc3c51f-0a26-4f2b-99bc-a1cd6450a5c7";
 	public static final String DATE_VERSION_CONVERSATION = "2016-09-20";
-	public static final String WORKSPACE_ID_CONVERSATION = "600d461a-ac5e-4972-bd18-24bdfb3c07a2";
+	public static final String WORKSPACE_ID_CONVERSATION = "263a1f22-4d05-4294-917b-180ee351760f";
 }
